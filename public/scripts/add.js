372250e617a159baec06254a1f7d5681d39474d2
onlylove <943788231@qq.com>
2016-12-12 22:12:17 +0800

    修改了添加讲师teacher的ajax请求

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -1,6 +1,14 @@
 define(function(require,exports,module){
     let $ = require('jquery');
     $('#addTeacher').on('submit',function(){
+        
+        let formData = $(this).serialize();
+
+        $.ajax({
+            url:'/teacher/add',
+            data:formData,
+            method:'post'
+        });
 
     });
 });
