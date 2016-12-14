085af4c3f1247af6b1a8166d5345b32b47da4446
onlylove <943788231@qq.com>
2016-12-14 19:05:33 +0800

    优化了一下Teachers的models 改为class

--- models/teachers.js
+++ models/teachers.js
@@ -1,9 +1,15 @@
 //处理teacher数据表的数据
 const db = require('../config/mysqlConnection');
+class Teachers{
+    constructor(){
 
-//下一步通过db处理数据
-exports.add = function(body,callback){
-    //body即表单数据
-    //插入操作
-    let sql = db.query('insert into `teacher` set ?',body,callback);
+    }
+    //下一步通过db处理数据
+    static add(body,callback){
+        //body即表单数据
+        //插入操作
+        let sql = db.query('insert into `teacher` set ?',body,callback);
+    }
 }
+
+module.exports = Teachers;
